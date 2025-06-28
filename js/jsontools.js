6af6d664f67f145696424a0ad924b78f5aa6e98e
Andreas Rodriguez Rivera <arrmaniac@gmail.com>
2025-06-28 11:11:03 +0200

    jsontools: Fix array expansion and convert JsonTools to exported object.

--- js/jsontools.js
+++ js/jsontools.js
@@ -5,53 +5,56 @@ const $JsonStringSource = $JsonExtractForExcelUseContainer.querySelector('[data-
 const $ConvertFromArrayButton = $JsonExtractForExcelUseContainer.querySelector('button[data-convert-from-array-button]');
 const $ResulttableContainer = $JsonExtractForExcelUseContainer.querySelector('[data-resulttable-container]');
 
+const JsonTools = {
+    processExtractForExcelUse: (event) => {
+        event.preventDefault();
+        let jsonString = $JsonStringSource.value;
+        let parsedJson = JSON.parse(jsonString);
+        console.log(parsedJson);
+
+        if(Array.isArray(parsedJson)) {
+            console.log('Is an Array.');
+            return this.processArrayForExcelUse(parsedJson);
+        }
+
+        console.log('Is NOT an Array.');
+    },
+    processArrayForExcelUse: (jsonArray = []) => {
+        let Columns = jsonArray.reduce((carry, item) => {
+            if(typeof item === 'object') {
+                Object.keys(item).forEach(key => carry.add(key));
+            }
+            return carry;
+        }, new Set());
+        let Headers = [...Columns];
+
+        let $table = Helper.getElement('table', [], $ResulttableContainer);
+        let $thead = Helper.getElement('thead', [], $table);
+        let $tbody = Helper.getElement('tbody', [], $table);
+
+        Headers.forEach(header => {
+            let $th = Helper.getElement('th', [], $thead);
+            $th.innerText = header;
+        });
+
+        jsonArray.forEach(element => {
+            let $tr = Helper.getElement('tr', [], $tbody);
+
+            if(typeof element === 'object') {
+                Headers.forEach(header => {
+                    let $td = Helper.getElement('td', [], $tr);
+                    $td.innerText = element[header];
+                });
+            } else {
+                let $td = Helper.getElement('td', [], $tr);
+                $td.innerText = element;
+            }
+        });
+    }
+};
+
 $JsonExtractForExcelUseContainer.addEventListener('click', event => {
-    if(event.target === $ConvertFromArrayButton) return processExtractForExcelUse(event);
+    if(event.target === $ConvertFromArrayButton) return JsonTools.processExtractForExcelUse(event);
 });
 
-function processExtractForExcelUse(event) {
-    event.preventDefault();
-    let jsonString = $JsonStringSource.value;
-    let parsedJson = JSON.parse(jsonString);
-    console.log(parsedJson);
-    
-    if(Array.isArray(parsedJson)) {
-        console.log('Is an Array.');
-        return processArrayForExcelUse(parsedJson);
-    }
-    
-    console.log('Is NOT an Array.');
-}
-
-function processArrayForExcelUse(jsonArray = []) {
-    let Columns = jsonArray.reduce((carry, item) => {
-        if(typeof item === 'object') {
-            Object.keys(item).forEach(key => carry.add(key));
-        }
-        return carry;
-    }, new Set());
-    let Headers = [Columns];
-    
-    let $table = Helper.getElement('table', [], $ResulttableContainer);
-    let $thead = Helper.getElement('thead', [], $table);
-    let $tbody = Helper.getElement('tbody', [], $table);
-    
-    Headers.forEach(header => {
-        let $th = Helper.getElement('th', [], $thead);
-        $th.innerText = header;
-    });
-    
-    jsonArray.forEach(element => {
-        let $tr = Helper.getElement('tr', [], $tbody);
-        
-        if(typeof element === 'object') {
-            Headers.forEach(header => {
-                let $td = Helper.getElement('td', [], $tr);
-                $td.innerText = element[header];
-            });
-        } else {
-            let $td = Helper.getElement('td', [], $tr);
-            $td.innerText = element;
-        }
-    });
-}
+export {JsonTools};
