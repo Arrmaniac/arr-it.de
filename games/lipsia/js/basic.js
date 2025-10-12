21e803aeec2fdbf80f0bb6942a2bd91a2204bb04
Andreas Rodriguez Rivera <arrmaniac@gmail.com>
2025-10-12 13:49:35 +0200

    Lipsia: try lock lanscape only in fullscreen mode.

--- games/lipsia/js/basic.js
+++ games/lipsia/js/basic.js
@@ -1,15 +1,15 @@
 const $fullscreenTrigger = document.querySelector('.heraldic-shield > img');
 
-screen.orientation.lock('landscape-primary').catch(error => {console.warn(error); alert(error);});
-
 $fullscreenTrigger.addEventListener('click', async event => {
     if(event.target.dataset.fullscreenTriggerStatus === 'off') {
         document.body.requestFullscreen().then(data => {
             event.target.dataset.fullscreenTriggerStatus = 'on';
+            screen.orientation.lock('landscape-primary').catch(error => {console.warn(error); alert(error);});
         });
     } else {
         document.exitFullscreen().then(data => {
             event.target.dataset.fullscreenTriggerStatus = 'off';
+            screen.orientation.unlock();
         });
     }
 });
