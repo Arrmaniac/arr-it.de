20b4f50a646933c4822ef500c7adc74d616905f9
Andreas Rodriguez Rivera <arrmaniac@gmail.com>
2025-10-12 14:05:53 +0200

    Lipsia: Build Slider behaviour.

--- games/lipsia/js/basic.js
+++ games/lipsia/js/basic.js
@@ -1,5 +1,20 @@
 const $fullscreenTrigger = document.querySelector('.heraldic-shield > img');
 
+function actuateSlider($sliderButton) {
+    let $slider = $sliderButton.closest('.slider');
+    let status = $slider.dataset.sliderStatus;
+    
+    if(status === 'out') {
+        $sliderButton.classList.replace('slider-position-out', 'slider-position-in');
+        $slider.classList.add('slider-in');
+        $slider.dataset.sliderStatus = 'in';
+    } else {
+        $sliderButton.classList.replace('slider-position-in', 'slider-position-out');
+        $slider.classList.remove('slider-in');
+        $slider.dataset.sliderStatus = 'out';
+    }
+}
+
 $fullscreenTrigger.addEventListener('click', async event => {
     if(event.target.dataset.fullscreenTriggerStatus === 'off') {
         document.body.requestFullscreen().then(data => {
@@ -12,5 +27,9 @@ $fullscreenTrigger.addEventListener('click', async event => {
             screen.orientation.unlock();
         });
     }
+});
+
+document.body.addEventListener('click', event => {
+    if(event.target.matches('.slider-button')) return actuateSlider(event.target);
 });
 
